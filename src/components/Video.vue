63873fa59e8d323601db9205aa20d6db0bb664e4
hthxzz <1023103937@qq.com>
2017-04-20 14:24:39 +0800

    Revert "first"
    This reverts commit 3da348362c539ef2eacd2dc8c5149df5cd4f8351.

--- src/components/Video.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>视频</div>
-</template>
-<script>
-	
-</script>
-<style lang="sass" scoped>
-	
-</style>
--- src/pages/TuiJianPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>aa</div>
-</template>
-<script>
-	
-</script>
-<style lang="sass" scoped>
-	
-</style>
